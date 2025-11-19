e2ae858ee29e1704f9a72c56c68817a68661312c
Jorge Luis Chuc Lopez <jorge.cl@campeche.tecnm.mx>
2025-11-19 15:52:26 -0600

    Sólo muestra la lista de compras

---
+++ ShopList.Gui/Models/ShopListItem.cs
@@ -0,0 +1,15 @@
+﻿namespace ShopList.Gui.Models
+{
+    public class ShopListItem
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public int Cantidad { get; set; }
+        public bool Comprado { get; set; } = false;
+
+        public override string ToString()
+        {
+            return $"{Nombre} ({Cantidad})";
+        }
+    }
+}
---
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ShopList.Gui.Models;
+using System.Collections.ObjectModel;
+
+namespace ShopList.Gui.ViewModels
+{
+    public class ShopListViewModel
+    {
+        public ObservableCollection<ShopListItem> ShopList { get; }
+
+        public ShopListViewModel()
+        {
+            ShopList = new ObservableCollection<ShopListItem>();
+            CargarDatos();
+        }
+
+        private void CargarDatos()
+        {
+            ShopList.Add(new ShopListItem()
+            {
+                Id = 1,
+                Nombre = "Pan de caja",
+                Cantidad = 1,
+                Comprado = false,
+            });
+            ShopList.Add(new ShopListItem()
+            {
+                Id = 2,
+                Nombre = "Leche",
+                Cantidad = 3,
+                Comprado = false,
+            });
+            ShopList.Add(new ShopListItem()
+            {
+                Id = 3,
+                Nombre = "Queso",
+                Cantidad = 100,
+                Comprado = false,
+            });
+        }
+    }
+}
