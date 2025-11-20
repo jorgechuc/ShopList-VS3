ea6896f45e413fe3d20b88cad92abcdb97b0ad76
Jorge Luis Chuc Lopez <jorge.cl@campeche.tecnm.mx>
2025-11-20 15:17:37 -0600

    Implementa MVVM para agregar elementos a la lista de compras

--- ShopList.Gui/ViewModels/ShopListViewModel.cs
+++ ShopList.Gui/ViewModels/ShopListViewModel.cs
@@ -1,16 +1,74 @@
 ﻿using ShopList.Gui.Models;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace ShopList.Gui.ViewModels
 {
-    public class ShopListViewModel
+    public class ShopListViewModel : INotifyPropertyChanged
     {
+        private string _nombreDelArticulo = string.Empty;
+        private int _cantidad = 1;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string NombreDelArticulo
+        {
+            get => _nombreDelArticulo;
+            set
+            {
+                if (_nombreDelArticulo != value)
+                {
+                    _nombreDelArticulo = value;
+                    OnPropertyChanged(nameof(NombreDelArticulo));
+                }
+            }
+        }
+
+        public int Cantidad
+        {
+            get => _cantidad;
+            set
+            {
+                if (_cantidad != value)
+                {
+                    _cantidad = value;
+                    OnPropertyChanged(nameof(Cantidad));
+                }
+            }
+        }
+
         public ObservableCollection<ShopListItem> ShopList { get; }
+
+        public ICommand AddShopListItemCommand { 
+            get; 
+            private set; 
+        }
 
         public ShopListViewModel()
         {
             ShopList = new ObservableCollection<ShopListItem>();
             CargarDatos();
+            AddShopListItemCommand = new Command(AddShopListItem);
+        }
+
+        public void AddShopListItem()
+        {
+            if (string.IsNullOrEmpty(NombreDelArticulo) || Cantidad <= 0)
+            {
+                return;
+            }
+            Random generador = new Random();
+            var item = new ShopListItem()
+            {
+                Id = generador.Next(),
+                Nombre = NombreDelArticulo,
+                Cantidad = this.Cantidad,
+                Comprado = false,
+            };
+            ShopList.Add(item);
+            NombreDelArticulo = string.Empty;
+            Cantidad = 1;
         }
 
         private void CargarDatos()
@@ -36,6 +94,14 @@ namespace ShopList.Gui.ViewModels
                 Cantidad = 100,
                 Comprado = false,
             });
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(
+                this, 
+                new PropertyChangedEventArgs(propertyName)
+                );
         }
     }
 }
